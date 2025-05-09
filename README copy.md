9496cb9a047a11a1795558579d2a23a84b3e2498
Edwin Pers <pers.edwin@gmail.com>
2025-05-09 17:03:29 -0400

    initial take on archlinux w/ gnome

---
+++ README copy.md
@@ -0,0 +1,7 @@
+# desktop-arch-xfce
+
+XFCE Desktop on Archlinux for WebMesh-VDI
+
+<https://github.com/kubevda/desktop-arch-xfce/pkgs/container/vdimage-arch-xfce>
+
+Track `latest` tag at your own risk!
---
+++ src/rootfs.base/entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+export HOME="${HOME:-/home/$USER}"
+
+export GROUP="${GROUP:-$USER}"
+
+echo "Setting up user account: ${USER}"
+
+groupadd \
+  --gid "${UID}" \
+    "${GROUP}"
+
+useradd \
+  --uid "${UID}" \
+  --gid "${UID}" \
+  --home-dir "${HOME}" \
+  --shell /bin/bash \
+    "${USER}"
+
+usermod \
+  -a -G \
+    adm,sys,wheel,video,audio,tty,users \
+      "${USER}"
+
+passwd -d "${USER}"
+
+echo "Setting up /run/user/${UID}"
+mkdir -p "/run/user/${UID}"
+chmod 0700 "/run/user/${UID}"
+chown "${USER}":"${GROUP}" "/run/user/${UID}"
+
+if [[ "${ENABLE_ROOT}" == "true" ]] ; then
+  echo "Allowing passwordless sudo for ${USER}"
+  echo "${USER} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+echo "Setting up user environment"
+
+# First setup VNC socket directory
+if [[ -z "${DISPLAY_SOCK_ADDR}" ]] ; then
+  if [[ -n "${VNC_SOCK_ADDR}" ]] ; then
+    export DISPLAY_SOCK_ADDR="${VNC_SOCK_ADDR}"
+  else
+    export DISPLAY_SOCK_ADDR="/run/user/${UID}/vnc.sock"
+  fi
+fi
+echo "Setting up VNC socket directory at $(dirname ${DISPLAY_SOCK_ADDR})"
+mkdir -pv "$(dirname ${DISPLAY_SOCK_ADDR})"
+chown -Rv "${USER}":"${GROUP}" "$(dirname ${DISPLAY_SOCK_ADDR})"
+
+echo "Granting ${USER} ownership of ${HOME}"
+chown -Rv "${USER}":"${GROUP}" "${HOME}"
+
+mkdir -pv /run/dbus
+
+/usr/bin/supervisord \
+  -n \
+  -c /etc/supervisor/supervisord.conf
